ec00e73f94594ff7f95953a485ad563f13da6f83
Pedro Hernandez <Pedro.lin.ka@gmail.com>
2020-02-13 13:22:52 -0500

    starting a new project

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+
+
+module.exports = {
+  mode: "production",
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.html$/, 
+        use: [
+                {
+                    loader: "html-loader", 
+                    options: {minimize: true}
+                }
+             ] 
+      },
+
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {presets: ['@babel/preset-env']}
+        }
+      },
+
+      {
+        test: /\.(png|svg|jpg|gif|ico)$/,
+        use: ['file-loader?name=[name].[ext]']
+      },
+
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+
+     ]
+  },
+
+  plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "index.html"}),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"})
+      ]
+};
