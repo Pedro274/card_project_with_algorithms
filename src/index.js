383c952b4530b95a6af3d2979ddb545bc72b1bdd
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-02-13 16:33:31 -0500

    lalska

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+// Import
 import './style/main.css'
 
+// Event Listeners
 document.getElementById("draw").addEventListener("click", function(event){
     event.preventDefault()
 });
 document.getElementById("sort").addEventListener("click", function(event){
     event.preventDefault()
 });
+
+// Inner HTML
+//document.getElementById("sortRow").innerHTML = "Heellooo";
+
+// BubbleSort
+var points = [40, 100, 1, 5, 25, 10];
+document.getElementById("sortRow").innerHTML = points;  
+
+function myFunction1() {
+  points.sort();
+  document.getElementById("sortRow").innerHTML = points;
+}
+function myFunction2() {
+  points.sort(function(a, b){return a - b});
+  document.getElementById("sortRow").innerHTML = points;
+}
