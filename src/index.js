11bfcd7dab477a834e7ed1aa2ef906f8f1b63c80
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-02-14 13:06:09 -0500

    random card

--- src/index.js
+++ src/index.js
@@ -27,6 +27,29 @@ document.getElementById("sort").addEventListener("click", function(event){
     event.preventDefault()
 });
 
+// Random Card
+
+const values = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+let cardValue = values[Math.floor(Math.random() * values.length)];
+const suits = ['&#9824;', '	&#9827;', '&#9829;', '&#9830;']
+let cardSuit = suits[Math.floor(Math.random() * suits.length)]
+
+  let card = `<div class="card">
+                  <div class="card-body">
+                    ${cardValue} ${cardSuit} ${cardValue}
+                  </div>
+                </div>`;
+
+ function dealCards(){
+   
+  let userSelection = +document.querySelector('#input').value
+  
+  for ( i = 0; i < userSelection; i++){
+    document.querySelector('#cardDisplay').innerHTML +=  card;
+  }
+ }
+
+
 // BubbleSort
 let array1 = [3, 2, 4, 5, 7, 6, 8, 9, 10,"A", "J", "Q", "K"];
 
@@ -51,3 +74,6 @@ const bubbleSort = (arr) => {
 };
 
 bubbleSort(array1)
+
+
+
